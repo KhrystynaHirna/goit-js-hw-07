9cb5e096e6b9050d7fbd4c150ce2362d6858b5fd
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-24 10:07:35 +0100

    add gallery items

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+let items;
+
+const gallery = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+// gallery.addEventListener('click', onGalleryItemsClick);
+
+function createGalleryItems(items) {
+    return items
+        .map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+ `;
+    })
+        .join('');
+
+}
+
+
+// function onGalleryItemsClick(ev) {
+//     ev.target.event;
+// }
